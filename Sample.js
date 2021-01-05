4f7924a25e5ca095d4155d4e133ee1ce9f9042cd
kuldeep216 <55515430+kuldeep216@users.noreply.github.com>
2021-01-05 18:11:07 +0530

    Add files via upload

---
+++ Sample.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var Startup = /** @class */ (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log("First Typescript Code");
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main();
---
+++ Sample.ts
@@ -0,0 +1,12 @@
+import { mainModule } from "process"
+
+class Startup
+{
+    public static main(): number
+    {
+        console.log("First Typescript Code");
+        return 0;
+    }
+}
+
+Startup.main();
